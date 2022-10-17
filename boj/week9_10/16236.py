fedfec8f2c84c35d4498f31beae2e4259842ac5f
dhLeoKim <win3ctrl@gmail.com>
2022-10-17 23:47:11 +0900

    1017 update

--- boj/week9_10/16236.py
+++ boj/week9_10/16236.py
@@ -2,19 +2,20 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-from collections import deque
-
 def bfs(start):
     global ret
-    visited = [[False]*N for _ in range(N)]
     i, j = start
-    queue = deque()
-    queue.append([i, j ,0])
+    visited = [[False]*N for _ in range(N)]
+    queue = [[i, j, 0]]
     visited[i][j] = True
     depth = 1
     di, dj = [-1, 0, 0, 1], [0, -1, 1, 0]
     while queue:
-        i, j, d = queue.popleft()
+        if queue[0][-1] == depth:
+            depth += 1
+            queue.sort()
+
+        i, j, d = queue.pop(0)
         if lst[i][j] and lst[i][j] < shark[0]:
             ret += d
             lst[i][j] = 0
@@ -23,17 +24,17 @@ def bfs(start):
                 shark[0] += 1
                 shark[1] = 0
             visited = [[False]*N for _ in range(N)]
+            queue = [[i, j, 0]]
             visited[i][j] = True
-            queue = deque()
-            queue.append([i, j, 0])
+            depth = 1
             continue
+        
         for k in range(4):
             ni, nj = i+di[k], j+dj[k]
             if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj] and lst[ni][nj] <= shark[0]:
                 visited[ni][nj] = True
                 queue.append([ni, nj, d+1])
         
-
 N = int(input())
 lst = []
 for i in range(N):
@@ -46,7 +47,6 @@ for i in range(N):
 
 ret = 0
 shark = [2, 0]
-
 bfs(start)
 
 print(ret)
